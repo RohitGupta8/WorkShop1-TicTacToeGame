f92995b40f98fcdc1a2900aa95f5c853d0b613f6
RohitGupta8 <therohitg@gmail.com>
2021-10-01 12:56:26 +0530

    ADD : Ability To Choose Letter X or O

--- src/main/java/com/bl/game/TicTacToeMain.java
+++ src/main/java/com/bl/game/TicTacToeMain.java
@@ -1,10 +1,16 @@
 package com.bl.game;
 
+import java.util.*;
+
 public class TicTacToeMain {
     /**
      * declare 1d array of char type
      */
     static char[] gameBoard = new char[10];
+
+    public static char Computerletter;
+    public static char Playerletter;
+    static Scanner scanner = new Scanner(System.in);
 
     /**
      * method for creating empty GameBoard
@@ -12,6 +18,32 @@ public class TicTacToeMain {
     public void createGameBoard() {
         for (int a = 1; a < gameBoard.length; a++) {
             gameBoard[a] = ' ';
+        }
+    }
+
+    /**
+     * method for choose Letter X or O
+     */
+    public void selectLetter() {
+        int flag = 0;
+        while (flag == 0) {
+            System.out.print("\nChoose your Letter\nPress 1 -> 'X'\nPress 2 -> 'O'\nEnter your Choice -> ");
+            int choice = scanner.nextInt();
+            if (choice == 1) {
+                Playerletter = 'X';
+                Computerletter = 'O';
+                System.out.println("\nPlayer letter -> " + Playerletter);
+                System.out.println("Computer letter -> " + Computerletter);
+                flag = 1;
+            } else if (choice == 2) {
+                Playerletter = 'O';
+                Computerletter = 'X';
+                System.out.println("\nPlayer letter -> " + Playerletter);
+                System.out.println("Computer letter -> " + Computerletter);
+                flag = 2;
+            } else {
+                System.out.println("\n>>>>>>> plz enter 1 or 2 <<<<<<<<<");
+            }
         }
     }
 
@@ -26,5 +58,6 @@ public class TicTacToeMain {
         System.out.println("#####################################\n      Welcome 2 TicTacToe Game\n#####################################\n");
 
         ticTacToeMain.createGameBoard();
+        ticTacToeMain.selectLetter();
     }
 }
